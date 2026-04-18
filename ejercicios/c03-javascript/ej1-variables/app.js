a9a88f2a2e6bb054ae436036fe145a1298e378b0
Ramiro Baez <ramibaezabruza@gmail.com>
2026-04-18 17:03:28 -0300

    Ejercicio 1: Variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,13 @@
+const nombre = "Ramiro Baez";
+const edad = 20;
+const materia = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+ 
+contador = contador + 1; 
+contador = contador + 1; 
+contador = contador + 1;
+
+console.log(`Valor final del contador: ${contador}`);
